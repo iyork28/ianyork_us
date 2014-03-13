41fe43f02456b9c00a2d58e7646a13f89dff605b
Ian York <iyork28@gmail.com>
2014-03-13 15:02:06 -0400

    Added basic homepage and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 IanyorkUs::Application.routes.draw do
+  
+  root 'static_pages#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
