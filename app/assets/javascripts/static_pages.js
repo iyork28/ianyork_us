e2115fd0d43823077b40505f8c36981be09197a6
Ian York <iyork28@gmail.com>
2014-07-23 15:00:05 -0400

    one popover
    only one popover shows at a time now yayaya

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,5 +3,7 @@
 $(document).ready(function() {
   $("[data-toggle=popover]").popover();
   
-  
+  $("[data-toggle=popover]").on('click', function (e) {
+    $("[data-toggle=popover]").not(this).popover('hide');
+  });
 });
